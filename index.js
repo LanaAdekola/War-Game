9bd63846e875f3f6294acdbc16b1b71295a7f8d8
LanaAdekola <adekolaadelana@gmail.com>
2023-03-27 07:46:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
+
+
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
             console.log(deckId)
         })
