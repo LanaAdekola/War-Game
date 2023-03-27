ccc4924b1d26a8a27bfdf3405b9ba82c40dbd298
LanaAdekola <adekolaadelana@gmail.com>
2023-03-27 07:42:54 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id
+            console.log(deckId)
+        })
+}
+
+newDeckBtn.addEventListener("click", handleClick)
